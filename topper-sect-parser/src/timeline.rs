fe8aa39cfdeb2c22b2a7af2275556c9359719d77
Seurimas <nicholasanderson5@gmail.com>
2022-10-20 14:08:43 -0500

    Update sect parser.

--- topper-sect-parser/src/timeline.rs
+++ topper-sect-parser/src/timeline.rs
@@ -25,21 +25,25 @@ lazy_static! {
     static ref OBSERVATIONS: Vec<String> = {
         let mut results = vec![];
         results.push(include_str!("../../triggers/Attack Observations.json").to_string());
+        results.push(include_str!("../../triggers/Bard Spoofs.json").to_string());
         results.push(include_str!("../../triggers/CombatActions.json").to_string());
         results.push(include_str!("../../triggers/Cures.json").to_string());
         results.push(include_str!("../../triggers/Hypnosis Spoofs.json").to_string());
         results.push(include_str!("../../triggers/Indorani Spoof.json").to_string());
         results.push(include_str!("../../triggers/Luminary Spoof.json").to_string());
         results.push(include_str!("../../triggers/Observations.json").to_string());
-        results.push(include_str!("../../triggers/Writhes.json").to_string());
-        results.push(include_str!("../../triggers/Simple Aff Messages.json").to_string());
         results.push(include_str!("../../triggers/Sentinel Spoof.json").to_string());
+        results.push(include_str!("../../triggers/Simple Aff Messages.json").to_string());
         results.push(include_str!("../../triggers/Subterfuge Spoofs.json").to_string());
         results.push(include_str!("../../triggers/Titan Lord Spoofs.json").to_string());
         results.push(include_str!("../../triggers/Wielding.json").to_string());
+        results.push(include_str!("../../triggers/Writhes.json").to_string());
         results.push(include_str!("../../triggers/Zealot Spoof.json").to_string());
         results.push(include_str!("../../triggers/Lists/Diagnose.json").to_string());
         results.push(include_str!("../../triggers/Lists/Wounds.json").to_string());
+        results.push(include_str!("../../triggers/Lists/AlliesEnemies.json").to_string());
+        results.push(include_str!("../../triggers/Lists/ColdRead.json").to_string());
+        results.push(include_str!("../../triggers/Lists/Pipes.json").to_string());
         results
     };
     static ref OBSERVER: ObservationParser<AetObservation> = {
