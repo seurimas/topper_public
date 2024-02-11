0596b0890f7022290ffa9c0a03b0d858d527a484
Nicholas <nicholasanderson5@gmail.com>
2024-02-11 08:46:17 -0600

    Clean up logs a bit.

--- topper-aetolia-mudlet/src/topper/web_ui.rs
+++ topper-aetolia-mudlet/src/topper/web_ui.rs
@@ -67,6 +67,8 @@ impl<'s> TopperModule<'s, AetTimeSlice, BattleStats> for WebModule {
                             if let Ok(mut explainer_page) = parser.parse_nodes(sect_log) {
                                 println!("Explainer page parsed successfully!");
                                 explainer_page.filter_out_from_body("Tells");
+                                explainer_page.filter_out_from_body("a sprawling venantium cuff");
+                                explainer_page.filter_out_from_body("an insignia of the Blades");
                                 explainer_page.filter_out_command("tell");
                                 explainer_page.filter_out_command("gtells");
                                 explainer_page.filter_out_command("gtstells");
